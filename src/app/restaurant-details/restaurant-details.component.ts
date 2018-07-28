e0919d24ff29cef438a6c818ac094366066bb80c
Gabriel Braga <gbraga.dev@gmail.com>
2018-07-28 18:47:00 -0300

    restaurants and shopping cart

---
+++ src/app/restaurant-details/restaurant-details.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core'
+import { ActivatedRoute } from '@angular/router'
+
+import { RestaurantsService } from '../restaurants/restaurants.service'
+
+import { Restaurant } from '../restaurants/restaurant/restaurant.model'
+
+@Component({
+  selector: 'mt-restaurant-details',
+  templateUrl: './restaurant-details.component.html'
+})
+export class RestaurantDetailsComponent implements OnInit {
+
+  restaurant : Restaurant
+
+  constructor(private restaurantsService : RestaurantsService,
+              private route : ActivatedRoute) { }
+
+  ngOnInit() {
+    this.restaurantsService.restaurantById(this.route.snapshot.params['id'])
+    .subscribe(restaurant => this.restaurant = restaurant)
+  }
+
+}
---
+++ src/app/restaurant-details/reviews/review.model.ts
@@ -0,0 +1,7 @@
+export interface Review {
+  comments: string
+  date: Date
+  name: string
+  rating: number
+  restaurantId: string
+}
